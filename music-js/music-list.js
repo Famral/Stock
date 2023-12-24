dd41ed72e6499fa36072961115f152014d721b95
Manish Meena <108734050+ConnectWithMan@users.noreply.github.com>
2023-12-24 23:45:43 +0530

    Add files via upload

---
+++ music-js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Kya karte The Sajna (Remix)",
+    artist: "Anuradha Paudwal",
+    img: "Kya-karte-The-Sajna",
+    src: "Kya-karte-The-Sajna"
+  },
+  {
+    name: "Hardcore Beats Music",
+    artist: "Manish - Famral",
+    img: "Hardcore-Beats-music2",
+    src: "Hardcore-Beats-music"
+  },
+  {
+    name: "Quick Beats - Music",
+    artist: "Manish Meena",
+    img: "Quick-Beats-Music",
+    src: "Quick-Beats-Music"
+  },
+  {
+    name: "First Music (Free Style)",
+    artist: "Famral Music - Manish",
+    img: "First-Music",
+    src: "First-Music"
+  },
+  {
+    name: "Shake Beat - Music",
+    artist: "Manish Meena",
+    img: "Shake-Beat-Music",
+    src: "Shake-Beat-Music"
+  },
+  {
+    name: "Power Dhol Celebration",
+    artist: "Manish Meena",
+    img: "Power-Dhol-Celebration-Music",
+    src: "Power-Dhol-Celebration-Music"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
